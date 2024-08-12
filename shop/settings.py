681a028cb4b9c6c000027a91bcd2814d52e58d33
Kroklik <pupzemli092@gmail.com>
2024-08-12 11:26:48 +0500

    'resolving_deployment_problem'

--- shop/settings.py
+++ shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1:8000', ".vercel.app", '127.0.0.1']
 
 # Application definition
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
